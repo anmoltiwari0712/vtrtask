9b11d9ef1378d53a368602a17776c235f158492b
Anmol Tiwari <96834701+anmoltiwari0712@users.noreply.github.com>
2023-01-02 14:17:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+ 
+import Camera from "./Camera";
+ 
+export default function App() {
+  return (
+    <div className="App">
+      <Camera />
+    </div>
+  );
+}
